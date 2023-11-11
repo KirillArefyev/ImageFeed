76d0c4367c578c649e18343b2d53a3409c94bcb0
Kirill Arefyev <arefyev2009@yandex.ru>
2023-11-11 12:24:03 +0300

    1. В ассеты добавлена иконка приложения, лого экрана запуска, набор цветов из макета 2. Сверстан экран запуска 3. Добавлено расширение для набора цветов 4. Изменена файловая структура приложения и размещение файлов

---
+++ ImageFeed/Extentions/UIColor+Extentions.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extentions.swift
+//  ImageFeed
+//
+//  Created by Кирилл on 11.11.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ifBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+    static var ifBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
+    static var ifBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.blue }
+    static var ifGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var ifRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var ifWhiteAlpha50: UIColor { UIColor(named: "YP White (Alpha 50)") ?? UIColor.lightGray }
+    static var ifWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white }
+}
